c6e2db40b6c5f6aa5abd469909f6d9c698fb43ab
Hanz Reyes <hanznreyes@gmail.com>
2024-03-15 10:31:45 +0000

    changed to bye

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
      public static void main(String[] a){
-          System.out.println("Hello World!");
-          System.out.println("Hello World!");
+          System.out.println("Goodbye!");
+          System.out.println("Goodbye!");
      }
 }
