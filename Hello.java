8ce99d70d36636eb172249972509f3a6a9bd18b7
Hanz Reyes <hanznreyes@gmail.com>
2024-03-15 10:32:52 +0000

    added int var

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
      public static void main(String[] a){
-          System.out.println("Hello World!");
+          int x = 10;
           System.out.println("Hello World!");
      }
 }
